e1f23121b8048fb32f2233838d7ab18534dd54c1
syamraj@gmail.com <syamraj@gmail.com@b94f81e3-bce7-34b6-c824-3e2d29604fdb>
2012-10-13 22:24:05 +0000

    With bank logo things are working login. failure and success

--- trunk/src/main/java/com/openbank/onlinebanking/controller/LoginController.java
+++ trunk/src/main/java/com/openbank/onlinebanking/controller/LoginController.java
@@ -2,38 +2,69 @@ package com.openbank.onlinebanking.controller;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.openbank.onlinebanking.blo.LoginService;
 import com.openbank.onlinebanking.blo.impl.LoginServiceImpl;
 import com.openbank.onlinebanking.form.LoginForm;
 
 @Controller
-@SessionAttributes
+//@SessionAttributes
+@RequestMapping("login")
 public class LoginController {
-
-	@RequestMapping(value = "/submitLogin", method = RequestMethod.POST)
-	public String login(@ModelAttribute("login") LoginForm loginForm, BindingResult result, Map model) {
-		
-		String forward = "login";
-		LoginService service = new LoginServiceImpl();
-		boolean isLoginSuccess = service.login(loginForm);
-		
-		if (isLoginSuccess) {
-			forward = "loginsuccess";
-		}
-		model.put("loginForm", loginForm);
-		return forward;
-	}
 	
-	@RequestMapping("/login")
-	public ModelAndView showContacts() {
-		return new ModelAndView("login", "command", new LoginForm());
-	}
+		@RequestMapping(method = RequestMethod.GET)
+		public String showForm(Map<String, LoginForm> model) {
+			LoginForm loginForm = new LoginForm();
+			model.put("loginForm", loginForm);
+			return "login";
+		}
+
+		@RequestMapping(method = RequestMethod.POST)
+		public String processForm(@Valid LoginForm loginForm, BindingResult result,
+				Map<String, LoginForm> model) {
+			String forward = "login";
+			LoginService service = new LoginServiceImpl();
+			boolean isLoginSuccess = service.login(loginForm);
+			
+			if (isLoginSuccess) {
+				forward = "loginsuccess";
+			} else {
+				loginForm = new LoginForm();
+			}
+			model.put("loginForm", loginForm);
+			return forward;
+		}
+
+//	@RequestMapping(value = "/submitLogin", method = RequestMethod.POST)
+//	public String login(@ModelAttribute("login") LoginForm loginForm, BindingResult result, Map<String, LoginForm> model) {
+//		
+//		String forward = "login.html";
+//		LoginService service = new LoginServiceImpl();
+//		boolean isLoginSuccess = service.login(loginForm);
+//		
+//		if (isLoginSuccess) {
+//			forward = "loginsuccess";
+//		}
+//		model.put("loginForm", loginForm);
+//		return forward;
+//	}
+//	
+//	@RequestMapping("/login")
+//	public ModelAndView showContacts() {
+//		return new ModelAndView("login", "command", new LoginForm());
+//	}
+	
+
+//	@RequestMapping(value= "/loginFailure", method = RequestMethod.POST)
+//	public String loginPage(@ModelAttribute("login") LoginForm loginForm, BindingResult result, Map<String, LoginForm> model) {
+//		model.put("loginForm", new LoginForm());
+//		return "login";
+//	}
+
 }
