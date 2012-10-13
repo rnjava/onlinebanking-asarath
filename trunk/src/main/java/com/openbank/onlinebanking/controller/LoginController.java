475ee755c76ea2f6844509b2942a130bfa67a0d5
syamraj@gmail.com <syamraj@gmail.com@b94f81e3-bce7-34b6-c824-3e2d29604fdb>
2012-10-13 05:48:12 +0000

    Working with tile

--- trunk/src/main/java/com/openbank/onlinebanking/controller/LoginController.java
+++ trunk/src/main/java/com/openbank/onlinebanking/controller/LoginController.java
@@ -1,40 +1,32 @@
 package com.openbank.onlinebanking.controller;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.openbank.onlinebanking.form.LoginForm;
 
 @Controller
-@RequestMapping("login.html")
+@SessionAttributes
 public class LoginController {
-	@RequestMapping(method = RequestMethod.GET)
-	public String showForm(Map model) {
-		LoginForm loginForm = new LoginForm();
-		model.put("loginForm", loginForm);
-		return "loginform";
-	}
 
-	@RequestMapping(method = RequestMethod.POST)
-	public String processForm(@Valid LoginForm loginForm, BindingResult result,
-			Map model) {
-		String userName = "UserName";
-		String password = "password";
-		if (result.hasErrors()) {
-			return "loginform";
-		}
-		loginForm = (LoginForm) model.get("loginForm");
-		if (!loginForm.getUserName().equals(userName)
-				|| !loginForm.getPassword().equals(password)) {
-			return "loginform";
-		}
-		model.put("loginForm", loginForm);
-		return "loginsuccess";
+	@RequestMapping(value = "/addContact", method = RequestMethod.POST)
+	public String addContact(@ModelAttribute("login")
+							LoginForm loginForm, BindingResult result) {
+		System.out.println("Inside addContact");
+		
+		System.out.println("First Name:" +loginForm.getUserName());
+		return "redirect:login";
 	}
-
+	
+	@RequestMapping("/login")
+	public ModelAndView showContacts() {
+		System.out.println("Inside Show contacts");
+		
+		return new ModelAndView("login", "command", new LoginForm());
+	}
 }
