8843bbf741beb6a572e9fa9a39221d48c71f06bd
syamraj <syamraj@gmail.com>
2012-11-09 05:25:33 +0000

    Works for two tenants.
    Dummy page added for staff and admin

--- readme.txt
+++ readme.txt
@@ -1,1 +1,22 @@
-db.dbname.load("C:\mongo_test_data_scripts.js")
+//db.dbname.load("C:\mongo_test_data_scripts.js")
+
+
+
+
+
+
+
+URL to login for users
+
+Tenant1 - HSBC
+
+http://localhost:8080/onlinebanking/user/login?tenantid=C63A953AE20D404F8BB1F225CBC82CF0
+
+
+Tenant 2 Bank of america
+http://localhost:8080/onlinebanking/user/login?tenantid=CCCEDA89EC74479AB5C20AF7ACCD789D
+
+
+Staff
+
+http://localhost:8080/onlinebanking/staff/stafflogin?tenantid=CCCEDA89EC74479AB5C20AF7ACCD789D
---
+++ src/main/java/com/openbank/onlinebanking/controller/user/LoginController.java
@@ -0,0 +1,73 @@
+package com.openbank.onlinebanking.controller.user;
+
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.openbank.onlinebanking.blo.LoginService;
+import com.openbank.onlinebanking.form.LoginForm;
+
+@Controller
+//@SessionAttributes
+@RequestMapping("/login")
+public class LoginController {
+	
+	private static Logger log = LoggerFactory.getLogger(LoginController.class);
+	
+	private LoginService loginService;
+	private AccountController accountController; 
+//	private ProfileService profileService;
+//	private AccountService accountService;
+		
+	@RequestMapping(method = RequestMethod.GET )
+	public String showForm(Map<String, LoginForm> model, @RequestParam(value = "tenantid") String tenantId) {
+			LoginForm loginForm = new LoginForm();
+			loginForm.setTenantId(tenantId);
+			model.put("loginForm", loginForm);
+			return "login";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView processForm(@Valid LoginForm loginForm, BindingResult result) {
+			
+			ModelAndView modelAndView = null;
+			String profileId = loginService.login(loginForm.getUserName(), loginForm.getTenantId(), loginForm.getPassword());
+			
+			log.debug("ProfileId for userId [{}] is [{}]", loginForm.getUserName(), profileId);
+			
+			if (profileId != null) {
+				modelAndView = accountController.getAccountOverview(profileId, loginForm.getTenantId()); 
+			} else {
+				modelAndView = new ModelAndView("login");
+				loginForm = new LoginForm();
+				modelAndView.addObject("form", loginForm);
+			}
+			
+			return modelAndView;
+		}
+
+	/**
+	 * @param loginService the loginService to set
+	 */
+	public void setLoginService(LoginService loginService) {
+		this.loginService = loginService;
+	}
+
+	/**
+	 * @param accountController the accountController to set
+	 */
+	public void setAccountController(AccountController accountController) {
+		this.accountController = accountController;
+	}
+
+	
+}
