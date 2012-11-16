66faf2bb3cb9899aba7f12570423a315e8e4ef13
syamraj <syamraj@gmail.com>
2012-11-16 06:22:29 +0000

    New Scripts

--- mongo_test_data_scripts.js
+++ mongo_test_data_scripts.js
@@ -1,46 +1,55 @@
 use banking;
 
 db.users.drop();
-db.users.save({ userId : 'sraj', password:'password', lastLoginDate:'10/12/2009', lastLoginTime:'10:24 AM ET',profileId:'3604F6E6-87E8-41C2-BF55-06C61F63CF99', tenantId:'C63A953AE20D404F8BB1F225CBC82CF0', createdDate:new Date('03/28/2009'), createdBy:'syam',   "role" : {
-    "primary" : "CUSTOMER"
-  } });
-db.users.save({ userId : 'sraj1', password:'password', lastLoginDate:'10/12/2009', lastLoginTime:'10:24 AM ET',profileId:'C3B62A95-2F72-4253-90F2-8A971B54A63C', tenantId:'CCCEDA89EC74479AB5C20AF7ACCD789D', createdDate:new Date('03/28/2009'), createdBy:'syam', "role" : {
-    "primary" : "CUSTOMER"
-  }});
 
-  db.users.save({ userId : 'admin', password:'password', lastLoginDate:'10/12/2009', lastLoginTime:'10:24 AM ET',profileId:'C3B62A95-2F72-4253-90F2-8A971B54A63C', tenantId:'039E8ED1-313A-412B-9ED4-DEE675FF544A', createdDate:new Date('03/28/2009'), createdBy:'syam', "role" : {
-    "primary" : "TENANT-ADMIN"
-  }});
-  
- db.profile.save({ profileId:'C3B789KK95-2F72-4253-90F2-8A971B54A63C', firstName:'Syam', lastName:'Raj', dateOfBirth:new Date('08/02/1981'), sex:'M', tenantId:'039E8ED1-313A-412B-9ED4-DEE675FF544A', createdDate:new Date('03/28/2009'), createdBy:'syam' });
- 
- db.users.save( 
-{
-  "userId" : "sraj2",
-  "password" : "password",
+db.users.save({
+  "userId" : "admin",
+  "password" : "admin",
   "lastLoginDate" : "10/12/2009",
   "lastLoginTime" : "10:24 AM ET",
-  "profileId" : "C3B62A95-2F72-4253-90F2-8A971B54A63C",
-  "tenantId" : "CCCEDA89EC74479AB5C20AF7ACCD789D",
+  "profileId" : "C3B789KK95-2F72-4253-90F2-8A971B54A63C",
+  "tenantId" : "039E8ED1-313A-412B-9ED4-DEE675FF544A",
   "createdDate" : ISODate("2009-03-28T07:00:00Z"),
-  "createdBy" : "syam",
+  "createdBy" : "anitha",
   "role" : {
-    "primary" : "staff",
-    "secondary" : "staff"
+    "primary" : "TENANT-ADMIN"
   }
 }
 );
-db.profile.drop();
-db.profile.save({ profileId:'3604F6E6-87E8-41C2-BF55-06C61F63CF99', firstName:'Syam', lastName:'Raj', dateOfBirth:new Date('08/02/1981'), sex:'M', tenantId:'C63A953AE20D404F8BB1F225CBC82CF0', createdDate:new Date('03/28/2009'), createdBy:'syam' });
-db.profile.save({ profileId:'C3B62A95-2F72-4253-90F2-8A971B54A63C', firstName:'Syam', lastName:'Raj', dateOfBirth:new Date('08/02/1981'), sex:'M', tenantId:'CCCEDA89EC74479AB5C20AF7ACCD789D', createdDate:new Date('03/28/2009'), createdBy:'syam' });
+
+db.profile.save({
+  "profileId" : "15AE1B5A-ACBD-4607-A374-AC92C60C3458",
+  "firstName" : "Anitha",
+  "lastName" : "Sarath",
+  "dateOfBirth" : ISODate("2012-10-10T07:00:00Z"),
+  "sex" : "M",
+  "tenantId" : "039E8ED1-313A-412B-9ED4-DEE675FF544A",
+  "createdDate" : ISODate("2012-11-16T03:06:26.603Z"),
+  "createdBy" : "Admin Admin",
+  "phone" : "9445768910",
+  "address" : "Anitha Software Solutions LTD, Kerala",
+  "emailAddress" : "admin@anithass.com"
+});
 
 
-db.account.drop();
-db.account.save({ accountNo:'1235687898',  type:'Savings', balance:'23456.90', profileId:'3604F6E6-87E8-41C2-BF55-06C61F63CF99', tenantId:'C63A953AE20D404F8BB1F225CBC82CF0', createdDate:new Date('03/28/2009'), createdBy:'syam' });
-db.account.save({ accountNo:'1235687810',  type:'Checkings', balance:'345.67', profileId:'3604F6E6-87E8-41C2-BF55-06C61F63CF99', tenantId:'C63A953AE20D404F8BB1F225CBC82CF0', createdDate:new Date('03/28/2009'), createdBy:'syam' });
+db.tenants.save({
+  "tenantId" : "C63A953AE20D404F8BB1F225CBC82CF0",
+  "tenantName" : "HSBC Bank",
+  "primaryContact" : "Tim Lee",
+  "phoneNo" : "8776753452",
+  "emailAddress" : "tim.lee@hsbc.com",
+  "address" : "HSBC, Hongkong",
+  "createdBy" : "anitha anitha",
+  "createdDate" : ISODate("2012-11-15T22:48:25.239Z")
+});
 
-db.transaction.drop();
-db.transaction.save({accountNo:'1235687810', date:new Date('03/28/2009'), description:'test', type:'CR', mode:'check', status:'Processed', amount:'234', availableBalance:'43434', tenantId:'C63A953AE20D404F8BB1F225CBC82CF0', createdDate:new Date('03/28/2009'), createdBy:'syam'});
-db.transaction.save({accountNo:'1235687810', date:new Date('05/28/2009'), description:'test', type:'CR', mode:'check', status:'Processed', amount:'2340', availableBalance:'434340', tenantId:'C63A953AE20D404F8BB1F225CBC82CF0', createdDate:new Date('03/28/2009'), createdBy:'syam'});
-db.transaction.save({accountNo:'1235687898', date:new Date('03/28/2009'), description:'test', type:'CR', mode:'check', status:'Processed', amount:'234', availableBalance:'43434', tenantId:'C63A953AE20D404F8BB1F225CBC82CF0', createdDate:new Date('03/28/2009'), createdBy:'syam'});
-db.transaction.save({accountNo:'1235687898', date:new Date('05/28/2009'), description:'test', type:'CR', mode:'check', status:'Processed', amount:'2340', availableBalance:'434340', tenantId:'C63A953AE20D404F8BB1F225CBC82CF0', createdDate:new Date('03/28/2009'), createdBy:'syam'});
+db.tenants.save({
+  "tenantId" : "CCCEDA89EC74479AB5C20AF7ACCD789D",
+  "tenantName" : "Citi Bank",
+  "primaryContact" : "Thomas Mathew",
+  "phoneNo" : "9008753452",
+  "emailAddress" : "tmathew@citibank.com",
+  "address" : "Citibank, Mumbai India",
+  "createdBy" : "anitha anitha",
+  "createdDate" : ISODate("2012-11-15T22:48:25.239Z")
+})
